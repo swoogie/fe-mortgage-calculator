bfa87787cb0a60a762192a3a5c434e93dd7c7a7f
JokubasD <jokubasdimsa@gmail.com>
2023-04-12 15:59:38 +0300

    routing updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MonthlyCalcComponent } from './monthly-calc/monthly-calc.component';
 // import * as path from 'path';
-import { NavigationComponent } from './navigation/navigation.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   // { path: 'login', component: LoginComponent },
   // { path: 'maximum-loan', component: MaximumLoanComponent },
-  // { path: 'monthly-payment', component: MonthlyPaymentComponent },
+  { path: 'monthly-payment', component: MonthlyCalcComponent },
   // { path: '**', component: NotFoundComponent }
 ];
 
