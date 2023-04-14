abc59499fbed511844133182e6ddaa0d9467c136
howard <howardtchienkoua@yahoo.com>
2023-04-14 23:25:58 +0300

    added-login-details

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+const fb = new FormBuilder().nonNullable;
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  value = 'Please insert your Username';
+  
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onSubmit() {
+    // Handle form submission
+  }
+}
+
+export class FirstNameValidator {
+  static validate(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (value && value.trim().length > 0) {
+        return null;
+      } else {
+        return { 'firstname': true };
+      }
+    };
+  }
+}
+
+export class LastNameValidator {
+  static validate(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (value && value.trim().length > 0) {
+        return null;
+      } else {
+        return { 'lastname': true };
+      }
+    };
+  }
 }
