523f172c7d10ce11a138d2afcda43f999f109a5f
howard <howardtchienkoua@yahoo.com>
2023-04-14 23:30:10 +0300

    added-login-angular-material-changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -23,6 +23,22 @@ export class LoginComponent {
 
   onSubmit() {
     // Handle form submission
+    if (this.form.invalid) {
+      return;
+    }
+  
+    // Get the form values
+    const firstName = this.form.get('firstname').value;
+    const lastName = this.form.get('lastname').value;
+    const email = this.form.get('email').value;
+    const password = this.form.get('password').value;
+  
+    // Do something with the form values (e.g. send an HTTP request)
+    console.log('Form submitted!');
+    console.log(`First Name: ${firstName}`);
+    console.log(`Last Name: ${lastName}`);
+    console.log(`Email: ${email}`);
+    console.log(`Password: ${password}`);
   }
 }
 
