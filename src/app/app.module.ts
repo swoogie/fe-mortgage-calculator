408e4a914c38b8114a2547220f2c7318c47d5eda
Ignas <ignasbieksa@gmail.com>
2023-04-16 18:53:45 +0300

    baseHref attribute added to angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -51,10 +50,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatRadioModule,
     HttpClientModule,
   ],
-  providers: [
-    MatSnackBarModule,
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
-  ],
+  providers: [MatSnackBarModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
