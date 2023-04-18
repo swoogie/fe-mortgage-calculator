d908eb1ba20ef3e0289ac7e0de51fc4b4b0b083c
howard <howardtchienkoua@yahoo.com>
2023-04-18 09:23:06 +0300

    login-form-update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,60 +1,96 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
+
+const fb = new FormBuilder();
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+export class LoginComponent implements OnInit{
+loginForm: FormGroup;
 
 
+constructor(
+  private fb: FormBuilder,
+  private authService: AuthService,
+  private router: Router,
+  private snackBar: MatSnackBar
+) {
+  
+}
+
+ngOnInit(): void {
+  this.loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
 
 
-export class LoginComponent {
-login: FormGroup<any>;
-form: any;
-
-
-emailFormControl = new FormControl('', [
-  Validators.required,
-  Validators.email,
-]);
-passwordFormControl = new FormControl('', [
-  Validators.required,
-]);
-  snackBar: any;
-  router: any;
-
-
-onLogin() {
-  if (this.emailFormControl.valid && this.passwordFormControl.valid) {
+  if (this.loginForm.valid) {
     // Do login logic here
-      const email = this.emailFormControl.value;
-      const password = this.passwordFormControl.value;
-  
-      // Call your authentication service to authenticate the user
-      this.authService.login(email, password).subscribe(
-        (response) => {
-          // If the authentication succeeds, redirect the user to the home page
-          this.router.navigate(['/home']);
-        },
-        (error) => {
-          // If the authentication fails, show an error message to the user
-          this.snackBar.open('Invalid email or password', 'Dismiss', {
-            duration: 5000,
-            horizontalPosition: 'center',
-            verticalPosition: 'bottom',
-          });
-        }
-      );
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    // Call your authentication service to authenticate the user
+    this.authService.login(email, password).subscribe(
+      (response) => {
+        // If the authentication succeeds, redirect the user to the home page
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        // If the authentication fails, show an error message to the user
+        this.snackBar.open('Invalid email or password', 'Dismiss', {
+          duration: 5000,
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom',
+        });
+      }
+    );
   }
+
 }
-constructor(private authService : AuthService){
+
+
+onSubmit() {
+  console.log(this.loginForm.value);
+}
+
+}
+// emailFormControl = new FormControl('', [
+//   Validators.required,
+//   Validators.email,
+// ]);
+// passwordFormControl = new FormControl('', [
+//   Validators.required,
+// ]);
   
-}
-
-}
-
+// onLogin() {
+//   if (this.emailFormControl.valid && this.passwordFormControl.valid) {
+//     // Do login logic here
+//       const email = this.emailFormControl.value;
+//       const password = this.passwordFormControl.value;
+  
+//       // Call your authentication service to authenticate the user
+//       this.authService.login(email, password).subscribe(
+//         (response) => {
+//           // If the authentication succeeds, redirect the user to the home page
+//           this.router.navigate(['/home']);
+//         },
+//         (error) => {
+//           // If the authentication fails, show an error message to the user
+//           this.snackBar.open('Invalid email or password', 'Dismiss', {
+//             duration: 5000,
+//             horizontalPosition: 'center',
+//             verticalPosition: 'bottom',
+//           });
+//         }
+//       );
+//   }
+// }
 
