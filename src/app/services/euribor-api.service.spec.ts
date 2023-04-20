2c40e68c01ab238f18539dd456273e8b21b32f98
Marina Klejevskaja <marina.klejevskaja@gmail.com>
2023-04-20 13:04:40 +0300

    euribor service updated

--- src/app/services/euribor-api.service.spec.ts
+++ src/app/services/euribor-api.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { EuriborApiService } from './euribor-api.service';
 
-describe('EuriborService', () => {
+describe('EuriborApiService', () => {
   let service: EuriborApiService;
 
   beforeEach(() => {
