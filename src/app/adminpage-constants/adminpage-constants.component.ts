318829de62084fd800b134b98254052a704aef78
JokubasD <jokubasdimsa@gmail.com>
2023-04-26 18:20:28 +0300

    added responsiveness and submit functionallity without the put request

--- src/app/adminpage-constants/adminpage-constants.component.ts
+++ src/app/adminpage-constants/adminpage-constants.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from '../services/api.service';
 import { Constants } from '../interfaces/constants';
 const fb = new FormBuilder().nonNullable;
@@ -32,20 +32,20 @@ export class AdminpageConstantsComponent {
       // dtiRatio: [''],
     }
     
-  );
+  )
   ngOnInit() {
     this.api.getConstants().subscribe((constants) => {
       console.log(constants);
       this.constants = constants
       this.cardItems = [{
         name: "Maximum loan term", 
-        desc: "The maximum loan term that the user can choose",
+        desc: "The maximum loan term that the user can choose.",
         formControlName: "maxLoanTerm",
         current: this.constants.maxLoanTerm,
       },
       {
         name: "Minimum loan term", 
-        desc: "The minimum loan term that the user can choose",
+        desc: "The minimum loan term that the user can choose.",
         formControlName: "minLoanTerm",
         current: this.constants.minLoanTerm,
       },
@@ -57,30 +57,33 @@ export class AdminpageConstantsComponent {
       },
       {
         name: "Loan amount percentage", 
-        desc: "The max loan amount in percentage of a users monthly income that they can take",
+        desc: "The max loan amount in percentage of a users monthly income that they can take.",
         formControlName: "loanAmountPercentage",
         current: `${this.constants.loanAmountPercentage * 100}%`,
       },
       {
         name: "Maximum kids", 
-        desc: "The maximum amount of kids that a user can have",
+        desc: "The maximum amount of kids that a user can have.",
         formControlName: "maxKids",
         current: this.constants.maxKids,
       },
       {
         name: "Maximum monthly obligations percentage", 
-        desc: "The maximum allowed monthly obligations (in percentage) that a user can have",
+        desc: "The maximum allowed monthly obligations (in percentage) that a user can have.",
         formControlName: "maxMonthlyObligationsPercentage",
         current: `${this.constants.maxMonthlyObligationsPercentage * 100}%`,
       },
       {
         name: "Maximum number of applicants", 
-        desc: "The maximum amount of applicants allowed when applying for a loan",
+        desc: "The maximum amount of applicants allowed when applying for a loan.",
         formControlName: "maxApplicants",
         current: this.constants.maxNumOfApplicants,
       },
     ]
     });
+  }
+  onSubmit() {
+    console.log(this.adminConstants.value)
   }
 }
 
