37e6b19a69c85e6f897fc05f31c1a35f18056c9d
JokubasD <jokubasdimsa@gmail.com>
2023-04-26 17:06:46 +0300

    made it so that the current values are fetched from our backend api

--- src/app/adminpage-constants/adminpage-constants.component.ts
+++ src/app/adminpage-constants/adminpage-constants.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { Constants } from '../interfaces/constants';
 const fb = new FormBuilder().nonNullable;
 
 @Component({
@@ -8,73 +10,77 @@ const fb = new FormBuilder().nonNullable;
   styleUrls: ['./adminpage-constants.component.scss']
 })
 export class AdminpageConstantsComponent {
-  constants: any[] = [{
-    name: "Maximum loan term", 
-    desc: "The maximum loan term that the user can choose",
-    formControlName: "maxLoanTerm",
-    current: 30,
-  },
-  {
-    name: "Minimum loan term", 
-    desc: "The minimum loan term that the user can choose",
-    formControlName: "minLoanTerm",
-    current: 1,
-  },
-  {
-    name: "Maximum real estate price", 
-    desc: "The maximum real estate price a user can provide",
-    formControlName: "maxRealEstatePrice",
-    current: 1000000,
-  },
-  {
-    name: "Minimum real estate price", 
-    desc: "The minimum real estate price a user can provide",
-    formControlName: "minRealEstatePrice",
-    current: 10000,
-  },
-  {
-    name: "Minimum loan amount", 
-    desc: "The minimum loan that a user can take",
-    formControlName: "minLoan",
-    current: 1000,
-  },
-  {
-    name: "Interest Rate margin", 
-    desc: "The difference between interest income and interest cost in terms of average assets.",
-    formControlName: "interestRateMargin",
-    current: "",
-  },
-  {
-    name: "Interest base rate", 
-    desc: "The interest rate that a central bank can change",
-    formControlName: "interestBaseRate",
-    current: "2.5%",
-  },
-  {
-    name: "LTV ratio", 
-    desc: "LTV ratio is a figure (expressed in the form of a percentage) that measures the appraised value of a home that you want to buy or refinance against the loan amount that you're seeking to borrow",
-    formControlName: "ltvRatio",
-    current: "",
-  },
-  {
-    name: "DTI ratio", 
-    desc: "The DTI ratio is all your monthly debt payments divided by your gross monthly income",
-    formControlName: "dtiRatio",
-    current: "",
-  },
+  constants: Constants;
+  cardItems: any[];
+  constructor (private api: ApiService) {}
 
-]
+  
   adminConstants = fb.group(
     {
       maxLoanTerm: [''],
       minLoanTerm: [''],
-      maxRealEstatePrice: [''],
-      minRealEstatePrice: [''],
-      minLoan: [''],
       interestRateMargin: [''],
-      interestBaseRate: [''],
-      ltvRatio: [''],
-      dtiRatio: [''],
+      loanAmountPercentage: [''],
+      maxKids: [''],
+      maxMonthlyObligationsPercentage: [''],
+      maxApplicants: [''],
+      // maxRealEstatePrice: [''],
+      // minRealEstatePrice: [''],
+      // minLoan: [''],
+      // interestBaseRate: [''],
+      // ltvRatio: [''],
+      // dtiRatio: [''],
     }
+    
   );
+  ngOnInit() {
+    this.api.getConstants().subscribe((constants) => {
+      console.log(constants);
+      this.constants = constants
+      this.cardItems = [{
+        name: "Maximum loan term", 
+        desc: "The maximum loan term that the user can choose",
+        formControlName: "maxLoanTerm",
+        current: this.constants.maxLoanTerm,
+      },
+      {
+        name: "Minimum loan term", 
+        desc: "The minimum loan term that the user can choose",
+        formControlName: "minLoanTerm",
+        current: this.constants.minLoanTerm,
+      },
+      {
+        name: "Interest Rate margin", 
+        desc: "The difference between interest income and interest cost in terms of average assets.",
+        formControlName: "interestRateMargin",
+        current: `${this.constants.interestRateMargin * 100}%`,
+      },
+      {
+        name: "Loan amount percentage", 
+        desc: "The max loan amount in percentage of a users monthly income that they can take",
+        formControlName: "loanAmountPercentage",
+        current: `${this.constants.loanAmountPercentage * 100}%`,
+      },
+      {
+        name: "Maximum kids", 
+        desc: "The maximum amount of kids that a user can have",
+        formControlName: "maxKids",
+        current: this.constants.maxKids,
+      },
+      {
+        name: "Maximum monthly obligations percentage", 
+        desc: "The maximum allowed monthly obligations (in percentage) that a user can have",
+        formControlName: "maxMonthlyObligationsPercentage",
+        current: `${this.constants.maxMonthlyObligationsPercentage * 100}%`,
+      },
+      {
+        name: "Maximum number of applicants", 
+        desc: "The maximum amount of applicants allowed when applying for a loan",
+        formControlName: "maxApplicants",
+        current: this.constants.maxNumOfApplicants,
+      },
+    ]
+    });
+  }
 }
+
