d934e56f75d9eec7ec200f49ec71319c8111beb7
Ignas <ignasbieksa@gmail.com>
2023-04-27 17:03:29 +0300

    fixed conflicts

--- src/app/userpage/userpage.component.ts
+++ src/app/userpage/userpage.component.ts
@@ -4,16 +4,8 @@ import { UserAuthService } from '../services/user-auth.service';
 @Component({
   selector: 'app-userpage',
   templateUrl: './userpage.component.html',
-  styleUrls: ['./userpage.component.scss']
+  styleUrls: ['./userpage.component.scss'],
 })
 export class UserpageComponent {
-
-  constructor(private user_authService : UserAuthService ){
-
-  }
-
-  Logout(): void {
-      this.user_authService.logout();
-  }
-
+  constructor(private user_authService: UserAuthService) {}
 }
