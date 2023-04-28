5b6db48a7de543da1e70321e115104a7493f9a44
Ignas <ignasbieksa@gmail.com>
2023-04-28 16:47:35 +0300

    skibidi bop mm dada added constants service and fleshed out apps dab on em

---
+++ src/app/services/constants.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConstantsService {
+  constructor(private http: HttpClient) {}
+  private adminApiUrl = 'https://be-mortgage-calculator.onrender.com/api/v1';
+
+  updateConstants(constants) {
+    this.http
+      .put(`${this.adminApiUrl}/admin/constants`, constants, {
+        responseType: 'text',
+      })
+      .subscribe();
+  }
+}
