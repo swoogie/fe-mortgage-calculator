a0c82d2f1926630b2674f72938d0924a05035ee8
Ilze Rigerte <irigerte@gmail.com>
2023-04-29 13:07:35 +0300

    user email

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -26,4 +26,29 @@ export class ApiService {
   checkEmail(email: string): Observable<any> {
     return this.http.get(`${this.apiUrl}/users/check-email?email=${email}`);
   }
+
+  getApplicationForUser(userId: number): Observable<ApplicationData[]> {
+    return this.http.get<ApplicationData[]>(
+      `${this.apiUrl}/users/${userId}/applications`
+    );
+  }
+
+  // listApplications(newConstants: any): Observable<ApplicationData[]> {
+  //   return this.http.put<ApplicationData[]>(
+  //     `${this.apiUrl}/users/{userId}/applications`,
+  //     newConstants,
+  //     {
+  //       headers: this.requestHeader,
+  //     }
+  //   );
+  // }
+  // listApplications(): Observable<ApplicationData[]> {
+  //   const headers = new HttpHeaders({
+  //     Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+  //   });
+  //   return this.http.get<ApplicationData[]>(
+  //     this.apiUrl + 'users/{userId}/applications',
+  //     { headers }
+  //   );
+  // }
 }
--- src/app/userpage/userpage.component.ts
+++ src/app/userpage/userpage.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApplicationData } from '../interfaces/application-data';
+import { ApiService } from '../services/api.service';
 import { UserAuthService } from '../services/user-auth.service';
 
 @Component({
@@ -6,6 +10,33 @@ import { UserAuthService } from '../services/user-auth.service';
   templateUrl: './userpage.component.html',
   styleUrls: ['./userpage.component.scss'],
 })
-export class UserpageComponent {
-  constructor(private user_authService: UserAuthService) {}
+export class UserpageComponent implements OnInit {
+  applications: ApplicationData[];
+  email: string;
+
+  constructor(
+    private user_authService: UserAuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit() {
+    this.user_authService.currentUserEmail.subscribe((email) => {
+      this.email = email;
+      console.log(email);
+    });
+    // this.apiService.listApplications().subscribe((data) => {
+    //   this.applications = data;
+    // });
+  }
+
+  handleApplicationDisplay() {
+    const userId: number = +this.route.snapshot.paramMap.get('id');
+
+    this.apiService.getApplicationForUser(userId).subscribe((data) => {
+      this.applications = data;
+    });
+  }
 }
