f828a29ef228f4b95270f7080d3e9caaf8cf1344
Marina Klejevskaja <marina.klejevskaja@gmail.com>
2023-04-30 00:17:26 +0300

    sort by columns in admin applications dash

--- src/app/adminpage-applications/adminpage-applications.component.ts
+++ src/app/adminpage-applications/adminpage-applications.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
-import { ApplicationService } from '../services/application.service';
-import { HttpClient } from '@angular/common/http';
-import { NumberValueAccessor } from '@angular/forms';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {animate, state, style, transition, trigger,} from '@angular/animations';
+import {ApplicationService} from '../services/application.service';
+import {HttpClient} from '@angular/common/http';
+import { MatSort } from '@angular/material/sort';
+import {MatTableDataSource} from "@angular/material/table";
+
 
 @Component({
   selector: 'app-adminpage-applications',
@@ -16,8 +12,8 @@ import { NumberValueAccessor } from '@angular/forms';
   styleUrls: ['./adminpage-applications.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
-      state('expanded', style({ height: '*' })),
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
       transition(
         'expanded <=> collapsed',
         animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
@@ -29,10 +25,15 @@ export class AdminpageApplicationsComponent implements OnInit {
   constructor(
     private applicationService: ApplicationService,
     private http: HttpClient
-  ) {}
+  ) {
+  }
 
-  dataSource: Application[] = [];
+  // dataSource: Application[] = [];
+  dataSource = new MatTableDataSource<Application>();
+
   applicationData: Application[] = [];
+  @ViewChild(MatSort) sort: MatSort;
+
   ngOnInit(): void {
     this.applicationService.getAllApplications().subscribe({
       next: (res: any) => {
@@ -52,9 +53,10 @@ export class AdminpageApplicationsComponent implements OnInit {
             personalNumber: appl.personalNumber,
           });
         });
-        this.dataSource = this.applicationData;
+        this.dataSource.data = this.applicationData;
+        this.dataSource.sort = this.sort;
       },
-      error: (err) => (this.dataSource = FALLBACK_DATA),
+      error: (err) => (this.dataSource.data = FALLBACK_DATA),
     });
   }
 
@@ -66,10 +68,12 @@ export class AdminpageApplicationsComponent implements OnInit {
     application.status = 'REJECTED';
     this.applicationService.setApplicationStatus(application.id, 'REJECTED');
   }
+
   approve(application: any) {
     application.status = 'APPROVED';
     this.applicationService.setApplicationStatus(application.id, 'APPROVED');
   }
+
   postpone(application: any) {
     application.status = 'IN_PROGRESS';
     this.applicationService.setApplicationStatus(application.id, 'IN_PROGRESS');
