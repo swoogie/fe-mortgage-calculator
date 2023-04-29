f1d3419cc51a38b5d0fe6b84d323ee20dcd97c88
Ilze Rigerte <irigerte@gmail.com>
2023-04-29 20:47:22 +0300

    formatting of user applications

--- src/app/userpage/userpage.component.ts
+++ src/app/userpage/userpage.component.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import jwtDecode from 'jwt-decode';
 import { Observable } from 'rxjs';
 import { ApplicationData } from '../interfaces/application-data';
 import { ApiService } from '../services/api.service';
 import { UserAuthService } from '../services/user-auth.service';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-userpage',
@@ -15,11 +16,30 @@ import { UserAuthService } from '../services/user-auth.service';
 export class UserpageComponent implements OnInit {
   applications: ApplicationData[];
   email: string;
+  statusMap = {
+    RECEIVED: {
+      text: 'Recieved',
+      class: 'received',
+    },
+    IN_PROGRESS: {
+      text: 'In Progress',
+      class: 'in-progress',
+    },
+    APPROVED: {
+      text: 'Approved',
+      class: 'approved',
+    },
+    REJECTED: {
+      text: 'Rejected',
+      class: 'rejected',
+    },
+  };
 
   constructor(
     private user_authService: UserAuthService,
     private route: ActivatedRoute,
-    private apiService: ApiService
+    private apiService: ApiService,
+    private cdRef: ChangeDetectorRef
   ) {
     const decodedToken: any = jwtDecode(localStorage.getItem('userToken'));
     const email = decodedToken.sub;
@@ -33,6 +53,7 @@ export class UserpageComponent implements OnInit {
   handleApplicationDisplay() {
     this.apiService.getApplicationForUser(this.email).subscribe((data) => {
       this.applications = data;
+      this.cdRef.detectChanges();
       console.log(data);
     });
   }
