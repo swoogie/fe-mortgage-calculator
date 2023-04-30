2572075a60c13592b4bac69dfb7db729c2864efa
Ilze Rigerte <irigerte@gmail.com>
2023-04-30 21:23:15 +0300

    progress bar out, some style in

--- src/app/userpage/userpage.component.ts
+++ src/app/userpage/userpage.component.ts
@@ -1,8 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
 import jwtDecode from 'jwt-decode';
-import { Observable } from 'rxjs';
 import { ApplicationData } from '../interfaces/application-data';
 import { ApiService } from '../services/api.service';
 import { UserAuthService } from '../services/user-auth.service';
@@ -18,8 +15,6 @@ export class UserpageComponent implements OnInit {
   applications: ApplicationData[];
   email: string;
   applicationStatus: string = '';
-  // activeStepIndex: number;
-  // currentStepIndex: number;
 
   constructor(
     private user_authService: UserAuthService,
@@ -32,7 +27,6 @@ export class UserpageComponent implements OnInit {
   }
 
   ngOnInit() {
-    // this.currentStepIndex = 0;
     this.handleApplicationDisplay();
   }
 
@@ -51,23 +45,10 @@ export class UserpageComponent implements OnInit {
     }
   }
 
-  // getStepIndex(application: ApplicationData): number {
-  //   const index = ['RECEIVED', 'IN_PROGRESS', 'APPROVED', 'REJECTED'].indexOf(
-  //     application.applicationStatus
-  //   );
-  //   return index >= 0 ? index : 0;
-  // }
-
   handleApplicationDisplay() {
     this.apiService.getApplicationForUser(this.email).subscribe((data) => {
       this.applications = data;
       this.applicationStatus = this.applications[0].applicationStatus;
-      // this.currentStepIndex = [
-      //   'RECEIVED',
-      //   'IN_PROGRESS',
-      //   'APPROVED',
-      //   'REJECTED',
-      // ].indexOf(this.applicationStatus);
       this.cdRef.detectChanges();
       console.log(data);
       console.log(this.applicationStatus);
