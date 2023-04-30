46e94d80237c2faf0a882db19fba8fefbe3fa8a9
Ilze Rigerte <irigerte@gmail.com>
2023-04-30 16:10:45 +0300

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { TokenInterceptor } from './interceptors/token.interceptor';
 import { AdminpageConstantsComponent } from './adminpage-constants/adminpage-constants.component';
 import { AdminpageApplicationsComponent } from './adminpage-applications/adminpage-applications.component';
 import { MatTableModule } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -78,6 +79,7 @@ import { MatTableModule } from '@angular/material/table';
     MatStepperModule,
     ChartModule,
     MatGridListModule,
+    MatExpansionModule,
   ],
   providers: [
     MatSnackBarModule,
--- src/app/userpage/userpage.component.ts
+++ src/app/userpage/userpage.component.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
   styleUrls: ['./userpage.component.scss'],
 })
 export class UserpageComponent implements OnInit {
+  panelOpenState = false;
   applications: ApplicationData[];
   email: string;
   applicationStatus: string = '';
